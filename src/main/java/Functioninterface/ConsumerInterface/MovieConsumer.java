62367273e2e5ce940f975956a5244a1c37c29d12
Adwiti789 <adwiti1975@gmail.com>
2019-03-10 22:51:21 -0700

    Movie use of Consumer

---
+++ src/main/java/Functioninterface/ConsumerInterface/MovieConsumer.java
@@ -0,0 +1,44 @@
+package Functioninterface.ConsumerInterface;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MovieConsumer {
+
+    public static void main(String[] args) {
+
+        List<Movie> movies = populate();
+
+        Consumer<Movie> c = m -> {
+            System.out.println("Movie Name : " + m.getName());
+            System.out.println("Hero Name : " + m.getHero());
+            System.out.println("Herione Name : " + m.getHerione());
+        };
+
+        for (Movie m : movies
+        ) {
+            c.accept(m);
+        }
+    }
+
+    static List<Movie> populate() {
+        Movie m1 = new Movie("Lagaan", "Aamir", "Smitha");
+        Movie m2 = new Movie("Bahubali", "Aamir", "Zeenat");
+        Movie m3 = new Movie("Chaubali", "Nirav", "Konkana");
+        Movie m4 = new Movie("Leta", "Pota", "Seemran");
+
+        return Arrays.asList(m1, m2, m3, m4);
+    }
+}
+
+@AllArgsConstructor
+@Getter
+class Movie {
+    private String name;
+    private String hero;
+    private String herione;
+}
